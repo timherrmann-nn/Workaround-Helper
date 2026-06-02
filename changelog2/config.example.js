169385c4759afd284a4feceeb657928471ef9553
timherrmann-bit <tim.herrmann@naughtynuts.de>
2026-06-02 14:16:27 +0200

    update 2

---
+++ changelog2/config.example.js
@@ -0,0 +1,10 @@
+window.__APP_CONFIG = {
+  firebase: {
+    apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
+    authDomain: "REPLACE_WITH_FIREBASE_AUTH_DOMAIN",
+    projectId: "REPLACE_WITH_FIREBASE_PROJECT_ID",
+    storageBucket: "REPLACE_WITH_FIREBASE_STORAGE_BUCKET",
+    messagingSenderId: "REPLACE_WITH_FIREBASE_SENDER_ID",
+    appId: "REPLACE_WITH_FIREBASE_APP_ID"
+  }
+};
